a33da7af07092f3be35f09d252c16388b937fc0a
princy1103 <princyjoshi5670@gmail.com>
2025-10-07 23:33:42 +0530

    Admin Panel

---
+++ ADMINPANEL/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Dashboard/>
+    </>
+  )
+}
+export default App
